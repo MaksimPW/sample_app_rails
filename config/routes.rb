3ad09af03b388d2763472204f9bc28ec1c498267
MaksimPW <ma.savichev@gmail.com>
2015-10-12 17:49:20 +0600

    rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
 
   get 'static_pages/about'
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,43 +2,39 @@ require 'rails_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+  let(:base_title) { "RoR" }
 
+  describe "Home page" do
+    before { visit root_path }
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
 
     it "should have the right title" do
-    	visit 'static_pages/home'
-    	expect(page).to have_title("Home")
+    	expect(page).to have_title("#{base_title} | Home")
   	end
   end
 
   describe "Help page" do
-
+    before { visit help_path }
   	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
   		expect(page).to have_content('Help')
   	end
 
-  	    it "should have the right title" do
-    	visit 'static_pages/help'
-    	expect(page).to have_title("Help")
+  	it "should have the right title" do
+    	expect(page).to have_title("#{base_title} | Help")
   	end
 
   end
 
   describe "About page" do
-
+    before { visit about_path }
   	it "should have the content 'About Us'" do
-  		visit '/static_pages/about'
   		expect(page).to have_content('About Us')
   	end
 
-  	    it "should have the right title" do
-    	visit 'static_pages/about'
-    	expect(page).to have_title("About")
+  	it "should have the right title" do
+    	expect(page).to have_title("#{base_title} | About")
   	end
 
   end
