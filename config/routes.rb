abfabb73f190884cd054808be2265c4a2bdefa56
MaksimPW <ma.savichev@gmail.com>
2015-10-14 16:11:02 +0600

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
-  get 'users/new'
-  match '/signup',    to: 'users#new',    via: 'get'
+  resources :users
 
   root  'static_pages#home'
+  
+  match '/signup',    to: 'users#new',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
 
